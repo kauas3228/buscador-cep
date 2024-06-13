781f7c3ec17072c00ba820a4fa513433cbc00054
kauas3228 <kauas3228@gmail.com>
2024-06-13 00:03:12 -0300

    feat: creating and styling information div

--- src/assets/Home/Home.tsx
+++ src/assets/Home/Home.tsx
@@ -7,11 +7,18 @@ export default function Home() {
     <div className='container'>
       <h1 className='pageTitle'>Buscador de CEP</h1>
       <div className='inputContainer'>
-        <input 
+        <input
           type='text'
           placeholder='Digite seu CEP..'
         />
         <button className='inputButton'><FaSearch /></button>
+      </div>
+      <div className="cepInformations">
+        <span className="CEP">CEP: 01310-930</span>
+        <span className="informations">Avenida Paulista</span>
+        <span className="informations">Complemento: 2100</span>
+        <span className="informations">Bela Vista</span>
+        <span className="informations">Sao Paulo - SP</span>
       </div>
     </div>
   )
