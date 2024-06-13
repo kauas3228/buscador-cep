28434b5334c17d96ece2f563221c3ba2af91442d
kauas3228 <kauas3228@gmail.com>
2024-06-12 23:55:28 -0300

    feat: input and button style

--- src/assets/Home/Home.tsx
+++ src/assets/Home/Home.tsx
@@ -1,9 +1,18 @@
+import { FaSearch } from "react-icons/fa";
+
 import './Home.css';
 
 export default function Home() {
   return (
     <div className='container'>
       <h1 className='pageTitle'>Buscador de CEP</h1>
+      <div className='inputContainer'>
+        <input 
+          type='text'
+          placeholder='Digite seu CEP..'
+        />
+        <button className='inputButton'><FaSearch /></button>
+      </div>
     </div>
   )
 }
